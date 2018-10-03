3eb224bf52b8c38fac05339710e6fee8f02739d3
Mårten Hakkestad <marha721@student.liu.se>
2018-10-03 13:09:20 +0200

    Looks right but does not work

--- src/components/footer/FilterFooter.js
+++ src/components/footer/FilterFooter.js
@@ -1,17 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+var twitter = true;
+var instagram = true;
+var  youtube = true;
 
+class FilterFooter extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      twitterS: true,
+      instagram: true,
+      youtube: true
+    };
 
+    this.changeTwitter() = this.changeTwitter().bind(this);
+    this.changeInstagram() = this.changeInstagram().bind(this);
+    this.changeYoutube() = this.changeYoutube().bind(this);
+  }
 
-const FilterFooter = () => {
-  return (
-      <div>
+  changeTwitter() {
+    this.setState({twitterS: !twitterS});
+    twitter != twitter;
+    console.log(twitter);
+  }
 
-    <FontAwesomeIcon icon="check-square" />
-      </div>
-  )
+  changeInstagram() {
+    instagram = !instagram;
+    console.log(instagram);
+  }
+
+  changeYoutube() {
+    youtube = !youtube;
+    console.log(youtube);
+  }
+
+  render() {
+    return (<div className="FiltFooter">
+      <FontAwesomeIcon icon={['fab', 'twitter']} className="footItem footStart" onClick={this.changeTwitter} style={{color: twitter? '#faa495' : '#E0E0EB'}}/>
+      <FontAwesomeIcon icon={['fab', 'instagram']} className="footItem footMid" onClick={this.changeInstagram} style={{color: instagram? '#faa495' : '#E0E0EB'}}/>
+      <FontAwesomeIcon icon={['fab', 'youtube']} className="footItem footEnd" onClick={this.changeYoutube} style={{color: youtube? '#faa495' : '#E0E0EB'}}/>
+    </div>)
+  }
 }
 
 export default FilterFooter
--- src/index.js
+++ src/index.js
@@ -9,9 +9,8 @@ import './styles/main.scss'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-library.add(fab, faCheckSquare)
+library.add(fab)
 
 
 
