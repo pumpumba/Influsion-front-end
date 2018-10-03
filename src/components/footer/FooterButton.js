e6ff8872f2180d3de1dfed9d15fc1a30dd61caba
Jonas Olaussen <jonasolaussen@gmail.com>
2018-10-03 22:48:02 +0200

    code cleanup, style improve

--- src/components/footer/FooterButton.js
+++ src/components/footer/FooterButton.js
@@ -5,9 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 class FooterButton extends React.Component {
 render (){
     return (
-        <div className="FooterButton">
           <Link to={this.props.link}> <FontAwesomeIcon icon={this.props.icon} /> </Link>
-        </div>
     )
   }
 }
--- src/components/footer/SubFooter.js
+++ src/components/footer/SubFooter.js
@@ -1,20 +1,18 @@
 import React, { Component }  from 'react';
-import { Link } from 'react-router-dom';
 import FooterButton from './FooterButton';
 
 
 
-class SubFooter extends React.Component {
- render() {
+const SubFooter = () => {
+
   return (
-      <div className="flex-container subFooter">
+      <div className="sub-footer">
           <FooterButton link={'/'} icon={'star'}/>
           <FooterButton link={'/feed'} icon={'heart'}/>
           <FooterButton link={'/search'} icon={'search'} />
           <FooterButton link={'/settings'} icon={'cogs'}/>
       </div>
   )
-  }
 }
 
 export default SubFooter
