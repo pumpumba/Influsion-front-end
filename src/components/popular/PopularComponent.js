9a035f5b4c8f38e2376eaae40a8e3a316b64d882
Jonas Olaussen <jonasolaussen@gmail.com>
2018-10-10 23:36:21 +0200

    improvements!

--- src/components/popular/PopularComponent.js
+++ src/components/popular/PopularComponent.js
@@ -14,8 +14,6 @@ class FeedComponent extends React.Component {
   }
 
   onClick() {
-    let newNextComponentOrder = 0;
-
     this.setState(prevState => ({
       open: !prevState.open
     }))
@@ -47,23 +45,24 @@ class FeedComponent extends React.Component {
           <div className='header'>
             <img src={this.props.data.user_profile_image_url} />
             <h3>{this.props.data.user_name}</h3>
-            {this.props.data.user_verified && <img className="verifiedIcon" src={require('../../../img/Twitter_Verified_Badge.svg')}/>}
+            {this.props.data.user_verified && <img className="verifiedIcon" src={require('../../../img/Twitter_Verified_Badge.svg')} />}
+            <a target_="blank" href={this.props.data.tweet_url}><FontAwesomeIcon className='icon' icon={['fab', 'twitter']} /></a>
           </div>
           <div className='content-container'>
-            <a target_="blank" href={this.props.data.tweet_url}><FontAwesomeIcon className='icon' icon={['fab', 'twitter']} /></a>
             <p className='tweet_text'>{this.props.data.tweet_text}</p>
             <img src={this.props.data.tweet_media[0]} />
             <div className='meta-data'>
-              <span className='no-of-likes'><FontAwesomeIcon className="metaIcon" icon={'heart'}/>
+              <span className='no-of-likes'><FontAwesomeIcon className="metaIcon" icon={'heart'} />
                 {this.props.data.tweet_favorite_count}</span>
-              <span className='no-of-retweets'> <FontAwesomeIcon className="metaIcon" icon={'retweet'}/>
+              <span className='no-of-retweets'> <FontAwesomeIcon className="metaIcon" icon={'retweet'} />
                 {this.props.data.tweet_retweet_count}</span>
-              <span className='time-stamp'><FontAwesomeIcon className="metaIcon" icon={'calendar-alt'}/>
-                <TimeAgo date={this.props.data.tweet_created_at}/>
+              <span className='time-stamp'><FontAwesomeIcon className="metaIcon" icon={'calendar-alt'} />
+                <TimeAgo date={this.props.data.tweet_created_at} />
               </span>
             </div>
           </div>
         </div>
+        <div className='blur-overlay'></div>
       </div>
     )
   }
