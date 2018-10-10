f8c245411b4b94f348060dc839fd3e262575c455
Mårten Hakkestad <marha721@student.liu.se>
2018-10-10 09:15:11 +0200

    Changed color of icons

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import Header from './../components/header/Header'
-import Footer from './../components/footer/Footer'
 import { Link } from 'react-router-dom'
 
 const Login = (props) => {
