34b456ec3a89f2f6226db013cc2d86387ac3a544
Jonas Olaussen <jonasolaussen@gmail.com>
2018-10-11 16:47:02 +0200

    connected dummy api

---
+++ src/components/feed/FeedComponent.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import TimeAgo from 'react-timeago'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+class FeedComponent extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+
+    return (
+      <div className='feed-component-wrapper'>
+        <div className='header'>
+          <img src={this.props.data.user_profile_image_url} />
+          <h3>{this.props.data.user_name}</h3>
+        </div>
+        <div className='content'>
+          <p className='tweet_text'>{this.props.data.tweet_text}</p>
+          <img src={this.props.data.media[0]} />
+        </div>
+        <div className='meta-data'>
+              <span className='no-of-likes'><FontAwesomeIcon className="metaIcon" icon={'heart'} />
+                {this.props.data.tweet_favorite_count}</span>
+              <span className='no-of-retweets'> <FontAwesomeIcon className="metaIcon" icon={'retweet'} />
+                {this.props.data.tweet_retweet_count}</span>
+              <span className='time-stamp'><FontAwesomeIcon className="metaIcon" icon={'calendar-alt'} />
+                <TimeAgo date={this.props.data.tweet_created_at} />
+              </span>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default FeedComponent
--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,15 +1,51 @@
 import React from 'react'
 import Header from './../components/header/Header'
 import Footer from './../components/footer/Footer'
+import FeedComponent from './../components/feed/FeedComponent'
 
-const Feed = () => {
-  return (
-    <div>
-      <Header title={'My Feed'}/>
-      <h1>Feed</h1>
-      <Footer />
-    </div>
-  )
+class Feed extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+
+
+  componentDidMount() {
+
+    fetch('http://40.127.101.155/twitter/content', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ filterType: ['user'], assetType: ['tweet'], filterValue: 'elonmusk' })
+    }).then(data => data.json())
+      .then(data => this.setState({ data }))
+  }
+
+  render() {
+
+    let { data } = this.state
+
+    let FeedContent = data.map(content => {
+      return <FeedComponent data={content}/>
+    })
+
+
+    return (
+      <div>
+        <Header title={'My Feed'} />
+        <main>
+          {FeedContent}
+        </main>
+        <Footer />
+      </div>
+    )
+  }
 }
+
 
 export default Feed
