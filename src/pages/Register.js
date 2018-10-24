dd8ff79b59614d3f1a2e51c3f89b41ff06c07c60
Jonas Olaussen <jonasolaussen@gmail.com>
2018-10-24 21:13:46 +0200

    rearregned componenteds and code cleanup

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,38 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-const Register = (props) => {
+class Register extends React.Component {
 
-    return (
-        <div className="register">
-            <h1 className="register-title">Register</h1>
-            <input placeholder="Username"></input>
-            <input placeholder="Password" type="password"></input>
-            <input placeholder="Email"></input>
-            <select name="sex">
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-            </select>
+    constructor(props) {
+        super(props)
+    }
 
-            <select name="Country">
-                <option value="Sweden">Sweden</option>
-                <option value="Norway">Norway</option>
-                <option value="Finland">Finland</option>
-                <option value="Denmark">Denmark</option>
-            </select>
+    registerNewUser(e) {
+        e.preventDefault()
+        console.log('hej')
+    }
 
-            <select name="City">
-                <option value="Stockholm">Stockholm</option>
-                <option value="Göteborg">Göteborg</option>
-                <option value="Linköping">Linköping</option>
-                <option value="Luleå">Luleå</option>
-            </select>
-
-            <Link to={'/feed'} className="register-button">
-                Register!
-            </Link>
-        </div>
-    )
+    render() {
+        return (
+            <form className="register">
+                <h1>inFlusion</h1>
+                <h2>Register</h2>
+                <input placeholder="Username"></input>
+                <input placeholder="Password" type="password"></input>
+                <input placeholder="Email"></input>
+                <select name="sex">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+                <button onClick={this.registerNewUser}>Register</button>
+            </form>
+        )
+    }
 }
 
 export default Register
