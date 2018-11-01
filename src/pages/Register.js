8f6ac069dbbaee3913d5ac7fac5fa09fb486433b
Jonas Olaussen <jonasolaussen@gmail.com>
2018-11-01 22:57:45 +0000

    very basic cond rendering applied

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -5,25 +5,44 @@ class Register extends React.Component {
 
     constructor(props) {
         super(props)
-
+        this.state = {
+            registerSuccsessfull: false,
+            registerUnsuccsessfull: false
+        }
         this.registerSuccsessfull = this.registerSuccsessfull.bind(this)
         this.registerUnsuccsessfull = this.registerUnsuccsessfull.bind(this)
     }
 
     registerSuccsessfull() {
-        console.log('suc')
+        this.setState({
+            registerSuccsessfull: true
+        })
     }
 
     registerUnsuccsessfull() {
-        console.log('usuc')
+        this.setState({
+            registerUnsuccsessfull: true
+        })
     }
 
     render() {
+        let objToRender;
+
+        if (this.state.registerSuccsessfull) {
+            objToRender = '<h1>suc</h1>'
+        } else if (this.state.registerUnsuccsessfull) {
+            objToRender = '<h1>Usuc</h1>'
+        } else {
+            objToRender =
+                <RegisterForm
+                    registerSuccsessfull={this.registerSuccsessfull}
+                    registerUnsuccsessfull={this.registerUnsuccsessfull}
+                />
+        }
         return (
-            <RegisterForm
-                registerSuccsessfull={this.registerSuccsessfull}
-                registerUnsuccsessfull={this.registerUnsuccsessfull}
-            />
+            <div>
+                {objToRender}
+            </div>
         )
     }
 }
