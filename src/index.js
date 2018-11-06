da2c4d8bee6f8f47b3119f6579854899d6bb564d
Jonas Olaussen <jonasolaussen@gmail.com>
2018-11-06 16:05:14 +0100

    now preventing user from acceessing app without login

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import Popular from './pages/Popular'
 import Feed from './pages/Feed'
 import InfluencerFeed from './pages/Influencerfeed'
@@ -16,32 +16,46 @@ library.add(fab, faCheckSquare, faStar, faHeart, faSearch, faCogs, faRetweet, fa
 
 class Index extends React.Component {
 
+    // 0 => not logged in
     constructor(props) {
         super(props)
         this.state = {
-          userId: 0
+            userId: 0
         }
         this.updateUserId = this.updateUserId.bind(this)
     }
 
     updateUserId(newId) {
-        this.setState({userId: newId})
+        this.setState({ userId: newId })
     }
 
+    checkIfLoggedIn() {
+        let curUrl = window.location.href
+        if(this.state.userId == 0 && !curUrl.includes('login')) {
+            let curUrl = window.location.href.split('/')
+            let newUrl = curUrl[0] + '//' + curUrl[2] + '/login'
+            console.log(newUrl)
+            window.location.replace(newUrl)
+        }
+    }
+
+    componentDidMount() {
+        this.checkIfLoggedIn()
+    }
 
     render() {
 
         return (
-                <Router>
-                    <div>
-                        <Route exact path='/' component={() => <Popular updateUserId={this.updateUserId} userId={this.state.userId} />}/>
-                        <Route path='/feed' component={() => <Feed updateUserId={this.updateUserId} userId={this.state.userId} />} />
-                        <Route path='/search' component={() => <Search updateUserId={this.updateUserId} userId={this.state.userId} />} />
-                        <Route path='/settings' component={() => <Settings updateUserId={this.updateUserId} userId={this.state.userId} />} />
-                        <Route path='/login' component={() => <Login updateUserId={this.updateUserId} userId={this.state.userId} />} />
-                        <Route path='/register' component={() => <Register updateUserId={this.updateUserId} userId={this.state.userId} />} />
-                    </div>
-                </Router>
+            <Router>
+                <div>
+                    <Route exact path='/' component={() => <Popular updateUserId={this.updateUserId} userId={this.state.userId} />} />
+                    <Route path='/feed' component={() => <Feed updateUserId={this.updateUserId} userId={this.state.userId} />} />
+                    <Route path='/search' component={() => <Search updateUserId={this.updateUserId} userId={this.state.userId} />} />
+                    <Route path='/settings' component={() => <Settings updateUserId={this.updateUserId} userId={this.state.userId} />} />
+                    <Route path='/login' component={() => <Login updateUserId={this.updateUserId} userId={this.state.userId} />} />
+                    <Route path='/register' component={() => <Register updateUserId={this.updateUserId} userId={this.state.userId} />} />
+                </div>
+            </Router>
         )
     }
 }
