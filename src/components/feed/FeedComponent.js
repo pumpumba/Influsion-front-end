57cbd1934de146db2798c829152cd8be40eedbb8
Jonas Olaussen <jonasolaussen@gmail.com>
2018-11-07 15:25:51 +0100

    changed feed component to new api

--- src/components/feed/FeedComponent.js
+++ src/components/feed/FeedComponent.js
@@ -20,33 +20,35 @@ class FeedComponent extends React.Component {
   }
 
   render() {
-    return (
-      <div className='feed-component-wrapper'>
-        <div className='header'>
-          <img src={props.data.user_profile_image_url} />
-          <a href={props.data.user_screen_name.toLowerCase()}> {props.data.user_name} </a>
-          <a target_="blank" href={props.data.tweet_url} className="twitter-icon"><FontAwesomeIcon className='icon' icon={['fab', 'twitter']} /></a>
+    if (this.props.data != null) {
+      return (
+        <div className='feed-component-wrapper'>
+          <div className='header'>
+            <img src={this.props.data.user_profile_image_url} />
+            <a href={this.props.data.user_screen_name.toLowerCase()}> {this.props.data.user_name} </a>
+            <a target_="blank" href={this.props.data.tweet_url} className="twitter-icon"><FontAwesomeIcon className='icon' icon={['fab', 'twitter']} /></a>
+          </div>
+          <div className='content'>
+            <p className='tweet_text'>{this.props.data.tweet_text}</p>
+            <img src={this.props.data.tweet_media[0]} />
+          </div>
+          <div className='meta-data'>
+            <span className='no-of-likes'><FontAwesomeIcon className="metaIcon" icon={'heart'} />
+              {this.props.data.tweet_favorite_count}
+            </span>
+            <span className='no-of-retweets'> <FontAwesomeIcon className="metaIcon" icon={'retweet'} />
+              {this.props.data.tweet_retweet_count}
+            </span>
+            <span className='time-stamp'><FontAwesomeIcon className="metaIcon" icon={'calendar-alt'} />
+              <TimeAgo date={this.props.data.tweet_created_at} />
+            </span>
+            <StopPropagation>
+              <FontAwesomeIcon icon={'heart'} className="follow_heart" onClick={this.changeHeart} data-state={this.state.heart && 'active'} />
+            </StopPropagation>
+          </div>
         </div>
-        <div className='content'>
-          <p className='tweet_text'>{this.props.data.tweet_text}</p>
-          <img src={this.props.data.tweet_media[0]} />
-        </div>
-        <div className='meta-data'>
-          <span className='no-of-likes'><FontAwesomeIcon className="metaIcon" icon={'heart'} />
-            {this.props.data.tweet_favorite_count}
-          </span>
-          <span className='no-of-retweets'> <FontAwesomeIcon className="metaIcon" icon={'retweet'} />
-            {this.props.data.tweet_retweet_count}
-          </span>
-          <span className='time-stamp'><FontAwesomeIcon className="metaIcon" icon={'calendar-alt'} />
-            <TimeAgo date={this.props.data.tweet_created_at} />
-          </span>
-          <StopPropagation>
-            <FontAwesomeIcon icon={'heart'} className="follow_heart" onClick={this.changeHeart} data-state={this.state.heart && 'active'} />
-          </StopPropagation>
-        </div>
-      </div>
-    )
+      )
+    } else return null
   }
 }
 
