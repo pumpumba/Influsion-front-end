d4b123fe2c73dc5713553a382235813050e5bb94
Linnea Sjögren <linnea.l.sjogren@gmail.com>
2018-11-07 13:33:25 +0100

    styling and front end functionality, no database connection

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -11,18 +11,49 @@ class Settings extends React.Component {
       isHidden: true,
       password: ''
       }
+      /*
+      this.deleteSuccsessfull = this.deleteSuccsessfull.bind(this)
+      this.deleteUnsuccsessfull = this.deleteUnsuccsessfull.bind(this)
+      this.resetState = this.resetState.bind(this)*/
     }
-  }
+
   toggleHidden () {
     this.setState({
       isHidden: !this.state.isHidden
     })
   }
-
-  deleteAccount () {
-    /db/delete_user
+  /*
+  deleteSuccsessfull() {
+      this.setState({
+          deleteSuccsessfull: true
+      })
   }
 
+  deleteUnsuccsessfull() {
+      this.setState({
+          deleteUnsuccsessfull: true
+      })
+  }
+
+  resetState() {
+      this.setState({
+          deleteUnsuccsessfull: false,
+          deleteSuccsessfull: false
+      })
+
+  deleteAccount (e) {
+    e.preventDefault()
+    fetch('http://40.127.101.155/db/delete_user', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ usrid: this.props.userID, password: e.password)
+    })
+    .then(response => response.json())
+    .then(response => (response.createSuccess) ? this.props.deleteSuccsessfull() : this.props.deleteUnsuccsessfull())
+  } */
 
   render() {
     return (
@@ -42,15 +73,13 @@ class Settings extends React.Component {
             <div className="DeleteComponent">
                   <h2> Are you sure that you want to delete your account? </h2>
                   <form>
-                      <h2>Register</h2>
-                      <input
-                          onChange={(e) => this.setState({ username: e.target.value })}
-                          placeholder="Username"
-                      >
-                      </input>
+                  <input
+                      onChange={(e) => this.setState({ password: e.target.value })}
+                      placeholder="Confirm Password"
+                      type="password"></input>
                   </form>
                   <div className="confirmOptions">
-                  <Link onClick={this.deleteAccount()}to='/delete' className="setting">Confirm</Link>
+                  <Link /*onClick={this.deleteAccount()}*/  to='/delete' className="setting">Confirm</Link>
                   <Link onClick={this.toggleHidden.bind(this)} to='/settings' className="setting" > Gosh no, cancel </Link>
                   </div>
             </div>
