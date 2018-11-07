a3bdc63712cf400442dc7f62c02f484726a2f088
Jonas Olaussen <jonasolaussen@gmail.com>
2018-11-07 13:25:08 +0100

    moved out component to new components

--- src/components/popular/PopularComponent.js
+++ src/components/popular/PopularComponent.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import TimeAgo from 'react-timeago'
 import { StopPropagation } from 'react-clickable';
+import PopularComponentClosedView from './popularSubComponents/PopularComponentClosedView'
+import PopularComponentExpandedView from './popularSubComponents/PopularComponentExpandedView'
 
 class PopularComponent extends React.Component {
     constructor(props) {
@@ -39,40 +41,25 @@ class PopularComponent extends React.Component {
                     data-state={this.state.open ? 'open' : 'closed'}
                     onClick={this.onClick}
                 >
-                    <div className='closed-view'>
-                        <div className='header'>
-                            <img src={this.props.data.platformcontent.user_profile_image_url} />
-                        </div>
-                        <StopPropagation>
-                            <a href={this.props.data.platformcontent.tweet_url}> <FontAwesomeIcon icon={['fab', 'twitter']} /> </a>
-                            <FontAwesomeIcon icon={'heart'} className="follow_heart" onClick={this.changeHeart} data-state={this.state.heart && 'active'} />
-                        </StopPropagation>
-                        {(this.props.data.platformcontent.tweet_media.length <= 0) ? <div className='text-wrapper'> <p className='tweet-text'>{this.props.data.platformcontent.tweet_text}</p> </div> : ''}
-                    </div>
-                    <div className='expanded-view'>
-                        <div className='header'>
-                            <img src={this.props.data.platformcontent.user_profile_image_url} />
-                            <a href={this.props.data.platformcontent.user_screen_name.toLowerCase()}> {this.props.data.platformcontent.user_name} </a>
-                            {this.props.data.platformcontent.user_verified && <img className="verifiedIcon" src={require('../../../img/Twitter_Verified_Badge.svg')} />}
-                            <a href={this.props.data.platformcontent.tweet_url}> <FontAwesomeIcon icon={['fab', 'twitter']} /> </a>
-                        </div>
-                        <div className='content-container'>
-                            <p className='tweet_text'>{this.props.data.platformcontent.tweet_text}</p>
-                            <img src={this.props.data.platformcontent.tweet_media[0]} />
-                            <div className='meta-data'>
-                                <span className='no-of-likes'><FontAwesomeIcon className="metaIcon" icon={'heart'} />
-                                    {this.props.data.platformcontent.tweet_favorite_count}</span>
-                                <span className='no-of-retweets'> <FontAwesomeIcon className="metaIcon" icon={'retweet'} />
-                                    {this.props.data.platformcontent.tweet_retweet_count}</span>
-                                <span className='time-stamp'><FontAwesomeIcon className="metaIcon" icon={'calendar-alt'} />
-                                    <TimeAgo date={this.props.data.platformcontent.tweet_created_at} />
-                                </span>
-                                <StopPropagation>
-                                    <FontAwesomeIcon icon={'heart'} className="follow_heart" onClick={this.changeHeart} data-state={this.state.heart && 'active'} />
-                                </StopPropagation>
-                            </div>
-                        </div>
-                    </div>
+                    <PopularComponentClosedView
+                        backgroundImage={this.props.data.platformcontent.tweet_media}
+                        userProfileImageUrl={this.props.data.platformcontent.user_profile_image_url}
+                        url={this.props.data.platformcontent.tweet_url}
+                        changeHeart={this.changeHeart}
+                        caption={this.props.data.platformcontent.tweet_text}
+                    />
+                    <PopularComponentExpandedView
+                        userProfileImageUrl={this.props.data.platformcontent.user_profile_image_url}
+                        userName={this.props.data.platformcontent.user_name}
+                        userVerified={this.props.data.platformcontent.user_verified}
+                        url={this.props.data.platformcontent.tweet_url}
+                        caption={this.props.data.platformcontent.tweet_text}
+                        img={this.props.data.platformcontent.tweet_media[0]}
+                        noOfLikes={this.props.data.platformcontent.tweet_favorite_count}
+                        noOfRetweet={this.props.data.platformcontent.tweet_retweet_count}
+                        timestamp={this.props.data.platformcontent.tweet_created_at}
+                        changeHeart={this.changeHeart}
+                    />
                     <div className='blur-overlay'></div>
                 </div>
             )
