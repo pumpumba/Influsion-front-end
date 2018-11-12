dd522880e911c2842bf8069267ee67de9640a3a0
Jonas Olaussen <jonasolaussen@gmail.com>
2018-11-12 10:30:30 +0100

    code cleanup footer

--- src/components/footer/FooterButton.js
+++ src/components/footer/FooterButton.js
@@ -3,9 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { NavLink } from 'react-router-dom'
 
 const FooterButton = (props) => {
-  return(
-      <NavLink exact to={props.link} activeStyle={{ color: '#faa495' }}> <FontAwesomeIcon icon={props.icon} /> </NavLink>
-  )
+    return (
+        <NavLink exact to={props.link} activeStyle={{ color: '#faa495' }}>
+            <FontAwesomeIcon icon={props.icon} />
+        </NavLink>
+    )
 }
 
 export default FooterButton
--- src/components/footer/SubFooter.js
+++ src/components/footer/SubFooter.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import FooterButton from './FooterButton';
+import React, { Component } from 'react'
+import FooterButton from './FooterButton'
 
 const SubFooter = () => {
 
