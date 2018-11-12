f3ccc7af56bf13f7e6881bb708374dc081e058b1
Jonas Olaussen <jonasolaussen@gmail.com>
2018-11-12 16:03:34 +0100

    improved search so it can be incrop to dev

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -5,43 +5,37 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Search extends React.Component {
 
-  constructor(props) {
-      super(props)
-      this.state = {
-          options: false,
-      }
-      this.optionOnClick = this.optionOnClick.bind(this)
-  }
-  optionOnClick() {
-      this.setState(prevState => ({
-          options: !prevState.options
-      }))
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            options: false,
+        }
+    }
 
     render() {
-      return (
-        <div>
-            <Header />
-            <main className='search-content'>
-                <form>
-                  <input className='searchInput'
-                      placeholder="Search"
-                  ></input>
-                  <button onClick={this.searchGo} >
-                    <FontAwesomeIcon icon={'search'} />
-                  </button>
-
-                </form>
-                <div className='info-text'>
-                  Welcome to the world of searching and all we can give
-                  <br/>
-                  <br/>
-                  Search for the username of your favorite influncer and experince the magic!
+        return (
+            <div>
+                <Header />
+                <main className='search-content'>
+                    <form className='search-input-wrapper'>
+                            <input
+                                className='searchInput'
+                                placeholder="Search"
+                            />
+                            <button >
+                                <FontAwesomeIcon icon={'search'} />
+                            </button>
+                    </form>
+                    <div className='info-text'>
+                        Welcome to the world of searching and all we can give
+                  <br />
+                        <br />
+                        Search for the username of your favorite influncer and experince the magic!
                 </div>
-            </main>
-            <Footer />
-        </div>
-      )
+                </main>
+                <Footer />
+            </div>
+        )
     }
 }
 
