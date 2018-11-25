87890f154f5ab6c28f6bf13ca796fd631170cf6b
Erik Stahl <e.stahl95@gmail.com>
2018-11-25 13:31:48 +0100

    The Node-server now works correctly for production use to serve a Single Page Application. Fixed problem with node_modules not copying to second part of Docker multi-stage build

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,15 @@ RUN yarn
 RUN yarn build
 
 FROM node:latest
-RUN npm install http-proxy -g
-RUN npm install express -g
 WORKDIR /src/usr/app
+RUN mkdir node_modules
+# add `/usr/src/app/node_modules/.bin` to $PATH
+##ENV PATH /usr/src/app/node_modules/.bin:$PATH
+#RUN npm install http-proxy -g
+RUN npm install express -g
+#RUN npm install
 COPY --from=0 /src/usr/app/dist .
+COPY --from=0 /src/usr/app/node_modules ./node_modules
 COPY server.js .
 EXPOSE 8080
 CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var express = require("express");
-var httpProxy = require("http-proxy");
+var path = require("path");
+var PORT = process.env.PORT || 8080;
 
 var app = express();
-var proxy = httpProxy.createProxyServer();
 
+app.use(express.static(__dirname));
 //This file is only intended for production use. It directs all routing to the same file, 
-//which is required for a Single Page Application. In this case create with React-Router
-app.all("/*", function (req, res) {
-  console.log("This server was started using a httpProxy to server a Single Page Application");
-  proxy.web(req, res, { target: "index.html" });
+//which is required for a Single Page Application. In this case created with React-Router
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 //create a server object:
-app.listen(8080); //the server object listens on port 8080
+console.log("This server was started on port", PORT, " inside the container");
+app.listen(PORT); //the server object listens on port 8080
