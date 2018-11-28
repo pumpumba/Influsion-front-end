8161c79a62b84a6b3c0c2230bb0d0dc43ec13294
Mårten Hakkestad <marha721@student.liu.se>
2018-11-28 10:07:37 +0100

    Fixed styling on button according to issue #127

--- src/components/register/RegisterAlert.js
+++ src/components/register/RegisterAlert.js
@@ -4,7 +4,7 @@ const RegisterAlert = (props) => {
 	return (
 		<div>
 			<h2>{props.title}</h2>
-			<a href={props.link} onClick={props.resetState}>{props.btnTxt}</a>
+			<button href={props.link} onClick={props.resetState} className='white-button'>{props.btnTxt}</button>
 		</div>
 	)
 }
