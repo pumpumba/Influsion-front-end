def8d7a4b9baeefb3e4c700e091a283ea7a6da3a
Linnea Sjögren <linnea.l.sjogren@gmail.com>
2018-11-28 08:57:07 +0100

    small style change on block-msg

--- src/components/admin/BlockSuggestionComponent.js
+++ src/components/admin/BlockSuggestionComponent.js
@@ -16,17 +16,8 @@ class BlockSuggestionComponent extends React.Component {
     })
   }
     render() {
-        let twitter = false
-        let instagram = false
-        let youtube = false
+
         let notFound = this.props.data.influencername === 'No matching influencers'
-
-        for (let i = 0; i < this.props.data.pfaccs.platformaccounts.length; i++) {
-            !twitter ? twitter = this.props.data.pfaccs.platformaccounts[i].platform === 'twitter' : ''
-            !instagram ? instagram = this.props.data.pfaccs.platformaccounts[i].platform === 'instagram' : ''
-            !youtube ? youtube = this.props.data.pfaccs.platformaccounts[i].platform === 'youtube' : ' '
-        }
-
         let profileImage
         if (!notFound) {
             profileImage = <img src={'https://pbs.twimg.com/media/DoGZoooXgAACCV3.jpg'} />
@@ -38,12 +29,12 @@ class BlockSuggestionComponent extends React.Component {
             <div className='search-header'>
                 {profileImage}
                 {this.props.data.influencername}
-                {this.state.blockSuccess && <p className='block-msg'>Block succesfull!</p>}
                 {this.state.blockFail && <p className='block-msg'> Please try again</p>}
-                <div className='search-platforms' style={{ justifyContent: 'right', alignItems: 'right', textalign: 'right' }}>
-                  <div className="blockButton" onClick={this.block}>
+                <div className='block-platforms'>
+                  {this.state.blockSuccess && <p className='block-msg'>Block succesfull!</p>}
+                  {!this.state.blockSuccess && <div className="blockButton" onClick={this.block}>
                     Block
-                  </div>
+                  </div> }
                 </div>
             </div>
         )
--- src/components/admin/BlockedListComponent.js
+++ src/components/admin/BlockedListComponent.js
@@ -30,7 +30,7 @@ class BlockedListComponent extends React.Component {
         <p>{this.props.index}</p>
         <img src="https://scontent-arn2-1.cdninstagram.com/vp/a775df49ed4c6966eeb7c43f83b15850/5C8B539B/t51.2885-19/11017586_953035901381515_1619288648_a.jpg"/>
         <p>{this.props.influencerName}</p>
-        {this.state.unblockSuccess && <p className='block-msg'>Unblock succesfull!</p>}
+        {this.state.unblockSuccess && <p className='block-platforms'>Unblock succesfull!</p>}
         {!this.state.unblockSuccess && <div className="unblockButton" onClick={this.unBlock}>
           Unblock
         </div> }
