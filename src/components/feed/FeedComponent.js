46ec1bf1b56087005c186d00265072be05dd51ac
Mårten Hakkestad <marha721@student.liu.se>
2018-12-04 11:20:27 +0100

    Fixed img url in follow feed. And fixed tweet text overflow in follow and popular

--- src/components/feed/FeedComponent.js
+++ src/components/feed/FeedComponent.js
@@ -29,6 +29,7 @@ class FeedComponent extends React.Component {
     render() {
         if (this.props.data != null && this.props.data.tweetText != "") {
             if(this.props.data.adid){
+                console.log(this.props.data.imgurl)
                 return(
                     <div className='feed-component-wrapper'>
                     <NavLink to={this.props.data.readmoreurl}>
